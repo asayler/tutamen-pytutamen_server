feb1cc4b4ebb397953d22b624c1cce8f03de70a6
Andy Sayler <andy.sayler@gmail.com>
2015-11-30 22:12:54 -0700

    Refactored to make server generator of child objects

--- tutamen_server/managment.py
+++ tutamen_server/managment.py
@@ -12,7 +12,6 @@ from pcollections import be_redis_atomic as ds_objects
 from pcollections import factories as ds_factories
 from pcollections import keys as ds_keys
 
-
 ### Objects ###
 
 class ManagmentServer(object):
@@ -23,68 +22,63 @@ class ManagmentServer(object):
         super().__init__()
 
         # Save Attrs
-        self._ds_driver = ds_driver
+        self._driver = ds_driver
 
+        # Setup Driver-Bound Factories
+        self._org_f_metadata = ds_factories.InstanceFactory(self._driver,
+                                                            Organization._type_metadata,
+                                                            ds_keys.StrKey)
+        self._org_f_accounts = ds_factories.InstanceFactory(self._driver,
+                                                            Organization._type_accounts,
+                                                            ds_keys.StrKey)
 
-    def organization_new(self, name):
-        return Organization.from_new(self._ds_driver, name)
-    def organization_existing(self, uid):
-        return Organization.from_existing(self._ds_driver, uid)
+    def organization_from_new(self, name):
+        """New Organization Constructor"""
+
+        uid = uuid.uuid4()
+        metadata = self._org_f_metadata.from_new(str(uid), {'name': name})
+        accounts = self._org_f_accounts.from_new(str(uid), [])
+        return Organization(uid, metadata, accounts)
+
+    def organization_from_existing(self, uid):
+        """Existing Organization Constructor"""
+
+        uid = uuid.UUID(uid)
+        metadata = self._org_f_metadata.from_existing(str(uid))
+        accounts = self._org_f_accounts.from_existing(str(uid))
+        return Organization(uid, metadata, accounts)
 
 class Organization(object):
 
-    def __init__(self, driver, uid, meta, accounts):
+    _type_uid = uuid.UUID
+    _type_metadata = ds_objects.MutableDictionary
+    _type_accounts = ds_objects.MutableList
+
+    def __init__(self, uid, metadata, accounts):
 
         # Call Parent
         super().__init__()
 
         # Check Args
-        if not isinstance(uid, uuid.UUID):
-            msg = "'uid' must be of type '{}', ".format(uuid.UUID)
+        if not isinstance(uid, self._type_uid):
+            msg = "'uid' must be of type '{}', ".format(self._type_uid)
             msg += "not '{}'".format(type(uid))
             raise TypeError(msg)
-        if not isinstance(meta, ds_objects.MutableDictionary):
-            msg = "'meta' must be of type '{}', ".format(ds_objects.MutableDictionary)
+        if not isinstance(metadata, self._type_metadata):
+            msg = "'metadata' must be of type '{}', ".format(self._type_metadata)
             msg += "not '{}'".format(type(meta))
             raise TypeError(msg)
-        if not isinstance(accounts, ds_objects.MutableList):
-            msg = "'accounts' must be of type '{}', ".format(ds_objects.MutableDictionary)
+        if not isinstance(accounts, self._type_accounts):
+            msg = "'accounts' must be of type '{}', ".format(self._type_accounts)
             msg += "not '{}'".format(type(accounts))
             raise TypeError(msg)
 
         # Save Attrs
-        self._ds_driver = driver
-        self._uuid = uuid
-        self._meta = meta
+        self._uid = uid
+        self._metadata = metadata
         self._accounts = accounts
-
-    @classmethod
-    def from_new(cls, driver, name):
-        """New Constructor"""
-
-        meta_f = ds_factories.InstanceFactory(driver, ds_objects.MutableDictionary, ds_keys.StrKey)
-        accounts_f = ds_factories.InstanceFactory(driver, ds_objects.MutableList, ds_keys.StrKey)
-        uid = uuid.uuid4()
-        d = {'name': name}
-        meta = meta_f.from_new(str(uid), d)
-        l = []
-        accounts = accounts_f.from_new(str(uid), l)
-
-        return cls(driver, uid, meta, accounts)
-
-    @classmethod
-    def from_existing(cls, driver, uid):
-        """Existing Constructor"""
-
-        meta_f = ds_factories.InstanceFactory(driver, ds_objects.MutableDictionary, ds_keys.StrKey)
-        accounts_f = ds_factories.InstanceFactory(driver, ds_objects.MutableList, ds_keys.StrKey)
-        uid = uuid.UUID(uid)
-        meta = meta_f.from_existing(str(uid))
-        accounts = accounts_f.from_existing(str(uid))
-
-        return cls(driver, uid, meta, accounts)
 
     def rem(self):
         """Delete Organization"""
-        self._meta.rem()
+        self._metadata.rem()
         self._accounts.rem()
