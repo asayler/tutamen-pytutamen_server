3996bcdb6ffd37c0b91191b1a2b7d067aa5c1918
Andy Sayler <andy.sayler@gmail.com>
2015-12-10 21:37:07 -0700

    Added Collections Tests

--- tests/server_storage_tests.py
+++ tests/server_storage_tests.py
@@ -66,11 +66,15 @@ class StorageServerTestCase(server_common.BaseTestCase):
         col = ss.collections_get(key=key)
         self.assertIsInstance(col, tutamen_server.storage.Collection)
         self.assertTrue(col.exists())
+        self.assertEqual(col.key, key)
+        self.assertEqual(col.uid, uid)
 
         # Test get (uuid)
         col = ss.collections_get(uid=uid)
         self.assertIsInstance(col, tutamen_server.storage.Collection)
         self.assertTrue(col.exists())
+        self.assertEqual(col.key, key)
+        self.assertEqual(col.uid, uid)
 
         # Cleanup
         col.destroy()
@@ -134,18 +138,16 @@ class StorageServerTestCase(server_common.BaseTestCase):
         # Delete Collection
         col.destroy()
 
-        # Test DNE
+        # Test DNE (key)
         self.assertFalse(ss.collections_exists(key=key))
+
+        # Test DNE (uuid)
+        self.assertFalse(ss.collections_exists(uid=uid))
 
         # Cleanup
         ss.destroy()
 
-class SecretTestCase(server_common.BaseTestCase):
-
-    def __init__(self, *args, **kwargs):
-
-        # Call Parent
-        super().__init__(*args, **kwargs)
+class StorageObjectTestBasis(server_common.BaseTestCase):
 
     def setUp(self):
 
@@ -158,10 +160,186 @@ class SecretTestCase(server_common.BaseTestCase):
     def tearDown(self):
 
         # Teardown Properties
-        self.ss.wipe()
+        self.ss.destroy()
 
         # Call Parent
         super().tearDown()
+
+class CollectionTestCase(StorageObjectTestBasis):
+
+    def test_init_create(self):
+
+        # Test Create
+        col = tutamen_server.storage.Collection(self.ss, create=True)
+        self.assertIsInstance(col, tutamen_server.storage.Collection)
+        self.assertTrue(col.exists())
+        self.assertTrue(self.ss.collections_exists(key=col.key))
+        self.assertIn(col.key, self.ss.collections_list())
+
+        # Cleanup
+        col.destroy()
+
+    def test_init_existing(self):
+
+        # Create Collection
+        col = tutamen_server.storage.Collection(self.ss, create=True)
+        key = col.key
+        uid = col.uid
+
+        # Test get (key)
+        col = tutamen_server.storage.Collection(self.ss, create=False, key=key)
+        self.assertIsInstance(col, tutamen_server.storage.Collection)
+        self.assertTrue(col.exists())
+        self.assertTrue(self.ss.collections_exists(key=col.key))
+        self.assertIn(col.key, self.ss.collections_list())
+        self.assertEqual(col.key, key)
+        self.assertEqual(col.uid, uid)
+
+        # Test get (uuid)
+        col = tutamen_server.storage.Collection(self.ss, create=False, uid=uid)
+        self.assertIsInstance(col, tutamen_server.storage.Collection)
+        self.assertTrue(col.exists())
+        self.assertTrue(self.ss.collections_exists(key=col.key))
+        self.assertIn(col.key, self.ss.collections_list())
+        self.assertEqual(col.key, key)
+        self.assertEqual(col.uid, uid)
+
+        # Cleanup
+        col.destroy()
+
+    def test_destroy(self):
+
+        # Create Collection
+        col = tutamen_server.storage.Collection(self.ss, create=True)
+
+        # Test Destroy
+        col.destroy()
+        self.assertFalse(col.exists())
+        self.assertFalse(self.ss.collections_exists(key=col.key))
+        self.assertNotIn(col.key, self.ss.collections_list())
+
+    def test_metadata(self):
+
+        # Create Collection
+        metadata = {"key1": "val1", "key2": "val2", "key3": "val3"}
+        col = tutamen_server.storage.Collection(self.ss, create=True, metadata=metadata)
+
+        # Test Metadata
+        self.assertEqual(col.metadata, metadata)
+
+        # Cleanup
+        col.destroy()
+
+    def test_secrets_create(self):
+
+        # Create Collection
+        col = tutamen_server.storage.Collection(self.ss, create=True)
+
+        # Create Secret
+        sec = col.secrets_create()
+        self.assertIsInstance(sec, tutamen_server.storage.Secret)
+        self.assertTrue(sec.exists())
+
+        # Cleanup
+        sec.destroy()
+        col.destroy()
+
+    def test_secrets_get(self):
+
+        # Create Collection
+        col = tutamen_server.storage.Collection(self.ss, create=True)
+
+        # Create Secret
+        sec = col.secrets_create()
+        key = sec.key
+        uid = sec.uid
+
+        # Test get (key)
+        sec = col.secrets_get(key=key)
+        self.assertIsInstance(sec, tutamen_server.storage.Secret)
+        self.assertTrue(sec.exists())
+        self.assertEqual(sec.key, key)
+        self.assertEqual(sec.uid, uid)
+
+        # Test get (uuid)
+        sec = col.secrets_get(uid=uid)
+        self.assertIsInstance(sec, tutamen_server.storage.Secret)
+        self.assertTrue(sec.exists())
+        self.assertEqual(sec.key, key)
+        self.assertEqual(sec.uid, uid)
+
+        # Cleanup
+        sec.destroy()
+        col.destroy()
+
+    def test_secrets_list(self):
+
+        # Create Collection
+        col = tutamen_server.storage.Collection(self.ss, create=True)
+
+        # List Secrets (Empty)
+        keys = col.secrets_list()
+        self.assertEqual(len(keys), 0)
+
+        # Create Secret
+        secs = []
+        for i in range(10):
+            secs.append(col.secrets_create())
+
+        # List Secrets (Full)
+        keys = col.secrets_list()
+        self.assertEqual(len(keys), len(secs))
+        for sec in secs:
+            self.assertTrue(sec.key in keys)
+
+        # Delete Secrets
+        for sec in secs:
+            sec.destroy()
+
+        # List Secrets (Empty)
+        keys = col.secrets_list()
+        self.assertEqual(len(keys), 0)
+
+        # Cleanup
+        col.destroy()
+
+    def test_secrets_exists(self):
+
+        # Create Collection
+        col = tutamen_server.storage.Collection(self.ss, create=True)
+
+        # Test DNE (key)
+        key = "fakekey"
+        self.assertFalse(col.secrets_exists(key=key))
+
+        # Test DNE (uuid)
+        uid = uuid.uuid4()
+        self.assertFalse(col.secrets_exists(uid=uid))
+
+        # Create Secret
+        sec = col.secrets_create()
+        key = sec.key
+        uid = sec.uid
+
+        # Test Exists (key)
+        self.assertTrue(col.secrets_exists(key=key))
+
+        # Test Exists (uuid)
+        self.assertTrue(col.secrets_exists(uid=uid))
+
+        # Delete Secret
+        sec.destroy()
+
+        # Test DNE (key)
+        self.assertFalse(col.secrets_exists(key=key))
+
+        # Test DNE (uuid)
+        self.assertFalse(col.secrets_exists(uid=uid))
+
+        # Cleanup
+        col.destroy()
+
+class SecretTestCase(server_common.BaseTestCase):
 
     def test_from_new(self):
 
