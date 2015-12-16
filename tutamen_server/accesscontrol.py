093bc175641189f68531e9b0b0f166f732270a2a
Andy Sayler <andy.sayler@gmail.com>
2015-12-15 18:00:02 -0700

    Added Authenticator class

--- tutamen_server/accesscontrol.py
+++ tutamen_server/accesscontrol.py
@@ -17,9 +17,11 @@ from . import datatypes
 ### Constants ###
 
 _KEY_AUTHORIZATIONS = "authorizations"
+_KEY_AUTHENTICATORS = "authenticators"
 
 _PREFIX_ACCESSCONTROLSERVER = "accesscontrolsrv"
 _PREFIX_AUTHORIZATION = "authorization"
+_PREFIX_AUTHENTICATOR = "authenticator"
 
 _POSTFIX_CLIENTUID = "clientuid"
 _POSTFIX_EXPIRATION = "expiration"
@@ -27,6 +29,8 @@ _POSTFIX_OBJPERM = "objperm"
 _POSTFIX_OBJTYPE = "objtype"
 _POSTFIX_OBJUID = "objuid"
 _POSTFIX_STATUS = "status"
+
+_POSTFIX_MODULE = "module"
 
 _NEW_STATUS = "pending"
 
@@ -43,10 +47,13 @@ class AccessControlServer(datatypes.PersistentObjectServer):
         # Setup Collections Index
         self._authorizations = datatypes.Index(self, key=_KEY_AUTHORIZATIONS, prefix=prefix,
                                                create=True, overwrite=False)
+        self._authenticators = datatypes.Index(self, key=_KEY_AUTHENTICATORS, prefix=prefix,
+                                               create=True, overwrite=False)
 
     def destroy(self):
 
         # Cleanup Indexes
+        self._authenticators.destroy()
         self._authorizations.destroy()
 
         # Call Parent
@@ -86,6 +93,41 @@ class AccessControlServer(datatypes.PersistentObjectServer):
 
         # Check membership
         return self._authorizations.is_member(key)
+
+    # Authenticator Methods #
+
+    def authenticators_create(self, **kwargs):
+
+        return Authenticator(self, create=True, **kwargs)
+
+    def authenticators_get(self, uid=None, key=None):
+
+        # Check Args
+        if not uid and not key:
+            raise TypeError("Requires either uid or key")
+
+        # Create
+        return Authenticator(self, create=False, key=key, uid=uid)
+
+    def authenticators_list(self):
+        return self._authenticators.members
+
+    def authenticators_exists(self, uid=None, key=None):
+
+        # Check Args
+        if not uid and not key:
+            raise TypeError("Requires either uid or key")
+        if uid:
+            datatypes.check_isinstance(uid, uuid.UUID)
+        if key:
+            datatypes.check_isinstance(key, str)
+
+        # Convert key
+        if not key:
+            key = str(uid)
+
+        # Check membership
+        return self._authenticators.is_member(key)
 
 class Authorization(datatypes.UUIDObject, datatypes.UserMetadataObject):
 
@@ -178,3 +220,50 @@ class Authorization(datatypes.UUIDObject, datatypes.UserMetadataObject):
     def status(self):
         """Return Status"""
         return self._status.get_val()
+
+class Authenticator(datatypes.UUIDObject, datatypes.UserMetadataObject):
+
+    def __init__(self, srv, create=False, overwrite=False,
+                 prefix=_PREFIX_AUTHENTICATOR,
+                 module=None, **kwargs):
+        """Initialize Authenticator"""
+
+        # Check Input
+        datatypes.check_isinstance(srv, AccessControlServer)
+        if create:
+            datatypes.check_isinstance(module, str)
+        if overwrite:
+            raise TypeError("Authenticator does not support overwrite")
+
+        # Call Parent
+        super().__init__(srv, create=create, overwrite=overwrite,
+                         prefix=prefix, **kwargs)
+
+        # Setup Status and Token
+        self._module = self._build_subobj(dso.String, _POSTFIX_MODULE,
+                                          create=create, value=module)
+
+        # Register with Server
+        if create:
+            self.srv._authenticators.add(self)
+        else:
+            if not self.srv.authenticators_exists(key=self.key):
+                msg = "Authorization not associated with srv"
+                raise TypeError(msg)
+
+    def destroy(self):
+        """Delete Authenticator"""
+
+        # Unregister with Server
+        self.srv._authenticators.remove(self)
+
+        # Cleanup Status and Token
+        self._module.rem()
+
+        # Call Parent
+        super().destroy()
+
+    @property
+    def module(self):
+        """Return Module"""
+        return self._module.get_val()
