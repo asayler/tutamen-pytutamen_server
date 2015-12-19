a0233e6b7154f8e20c3a4d984fd757dfaf756262
Andy Sayler <andy.sayler@gmail.com>
2015-12-18 21:27:04 -0700

    Removed authset

--- pytutamen_server/accesscontrol.py
+++ pytutamen_server/accesscontrol.py
@@ -18,12 +18,10 @@ from . import datatypes
 
 _KEY_AUTHORIZATIONS = "authorizations"
 _KEY_AUTHENTICATORS = "authenticators"
-_KEY_AUTHSETS = "authsets"
 
 _PREFIX_ACCESSCONTROLSERVER = "accesscontrolsrv"
 _PREFIX_AUTHORIZATION = "authorization"
 _PREFIX_AUTHENTICATOR = "authenticator"
-_PREFIX_AUTHSET = "authset"
 
 _POSTFIX_CLIENTUID = "clientuid"
 _POSTFIX_EXPIRATION = "expiration"
@@ -32,7 +30,6 @@ _POSTFIX_OBJTYPE = "objtype"
 _POSTFIX_OBJUID = "objuid"
 _POSTFIX_STATUS = "status"
 _POSTFIX_MODULE = "module"
-_POSTFIX_AUTHSETS = "authsets"
 _POSTFIX_AUTHENTICATORS = "authenticators"
 
 _NEW_STATUS = "pending"
@@ -52,7 +49,6 @@ class AccessControlServer(datatypes.PersistentObjectServer):
                                                create=True, overwrite=False)
         self._authenticators = datatypes.Index(self, key=_KEY_AUTHENTICATORS, prefix=prefix,
                                                create=True, overwrite=False)
-        self._authsets = datatypes.Index(self, key=_KEY_AUTHSETS, prefix=prefix,
                                          create=True, overwrite=False)
 
     def destroy(self):
@@ -60,7 +56,6 @@ class AccessControlServer(datatypes.PersistentObjectServer):
         # Cleanup Indexes
         self._authenticators.destroy()
         self._authorizations.destroy()
-        self._authsets.destroy()
 
         # Call Parent
         super().destroy()
@@ -135,25 +130,17 @@ class AccessControlServer(datatypes.PersistentObjectServer):
         # Check membership
         return self._authenticators.is_member(key)
 
-    # Authset Methods #
 
-    def authsets_create(self, **kwargs):
 
-        return Authset(self, create=True, **kwargs)
 
-    def authsets_get(self, uid=None, key=None):
 
         # Check Args
         if not uid and not key:
             raise TypeError("Requires either uid or key")
 
         # Create
-        return Authset(self, create=False, key=key, uid=uid)
 
-    def authsets_list(self):
-        return self._authsets.members
 
-    def authsets_exists(self, uid=None, key=None):
 
         # Check Args
         if not uid and not key:
@@ -168,7 +155,6 @@ class AccessControlServer(datatypes.PersistentObjectServer):
             key = str(uid)
 
         # Check membership
-        return self._authsets.is_member(key)
 
 class Authorization(datatypes.UUIDObject, datatypes.UserMetadataObject):
 
@@ -283,7 +269,6 @@ class Authenticator(datatypes.UUIDObject, datatypes.UserMetadataObject):
         # Setup Vars
         self._module = self._build_subobj(dso.String, _POSTFIX_MODULE,
                                           create=create, value=module)
-        self._authsets = self._build_subobj(dso.MutableSet, _POSTFIX_AUTHSETS,
                                             create=create, value=set())
 
         # Register with Server
@@ -300,12 +285,8 @@ class Authenticator(datatypes.UUIDObject, datatypes.UserMetadataObject):
         # Unregister with Server
         self.srv._authenticators.remove(self)
 
-        # Unregister with Authsets
-        for authset in self.authsets_by_obj():
-            authset.remove(self)
 
         # Cleanup Vars
-        self._authsets.rem()
         self._module.rem()
 
         # Call Parent
@@ -316,100 +297,37 @@ class Authenticator(datatypes.UUIDObject, datatypes.UserMetadataObject):
         """Return Module"""
         return self._module.get_val()
 
-    def authsets_by_key(self):
-        """Return Authset Memberships as Keys"""
-        return self._authsets.get_val()
 
-    def authsets_by_uid(self):
-        """Return Authset Memberships as UUIDs"""
-        return set([self._val_to_uid(key) for key in self._authsets])
 
-    def authsets_by_obj(self):
-        """Return Authset Memberships as Objects"""
-        return set([self._val_to_obj(key, obj=Authset) for key in self._authsets])
 
-class Authset(datatypes.UUIDObject, datatypes.UserMetadataObject):
 
     def __init__(self, srv, create=False, overwrite=False,
-                 prefix=_PREFIX_AUTHSET, **kwargs):
-        """Initialize Authset"""
 
         # Check Input
         datatypes.check_isinstance(srv, AccessControlServer)
         if create:
             pass
         if overwrite:
-            raise TypeError("Authenticator does not support overwrite")
 
         # Call Parent
         super().__init__(srv, create=create, overwrite=overwrite,
                          prefix=prefix, **kwargs)
 
         # Setup Vars
-        self._authenticators = self._build_subobj(dso.MutableSet, _POSTFIX_AUTHENTICATORS,
-                                                  create=create, value=set())
 
         # Register with Server
         if create:
-            self.srv._authsets.add(self)
         else:
-            if not self.srv.authsets_exists(key=self.key):
-                msg = "Authset not associated with srv"
                 raise TypeError(msg)
 
     def destroy(self):
-        """Delete Authenticator"""
 
         # Unregister with Server
-        self.srv._authsets.remove(self)
 
-        # Unregister with Authenticators
-        for actr in self.members_by_obj():
-            self.remove(actr)
 
-        # Cleanup Status and Token
-        self._authenticators.rem()
 
         # Call Parent
         super().destroy()
 
-    def members_by_key(self):
-        """Return Authenticator Members as string keys"""
-        return self._authenticators.get_val()
 
-    def members_by_uid(self):
-        """Return Authenticator Members as UUID objects"""
-        return set([self._val_to_uid(key) for key in self._authenticators])
 
-    def members_by_obj(self):
-        """Return Authenticator Members as Objects"""
-        return set([self._val_to_obj(key, obj=Authenticator) for key in self._authenticators])
-
-    def is_member(self, val):
-        """Return if Authenticator is member"""
-
-        # Process Val
-        key = self._val_to_key(val)
-
-        # Compute Membership
-        return key in self._authenticators
-
-    def add(self, val):
-        """Add Authenticator"""
-
-        # Process Val
-        actr = self._val_to_obj(val, obj=Authenticator)
-
-        # Add Authenticator
-        self._authenticators.add(actr.key)
-        actr._authsets.add(self.key)
-
-    def remove(self, val):
-        """Remove Authenticator"""
-
-        # Process Val
-        actr = self._val_to_obj(val, obj=Authenticator)
-
-        # Remove Authenticator
-        actr._authsets.discard(self.key)
-        self._authenticators.discard(actr.key)
