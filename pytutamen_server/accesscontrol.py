d30d3046060588a4768a100e2a2f92902451e17e
Andy Sayler <andy.sayler@gmail.com>
2015-12-18 21:30:04 -0700

    Added account

--- pytutamen_server/accesscontrol.py
+++ pytutamen_server/accesscontrol.py
@@ -19,11 +19,13 @@ from . import datatypes
 _KEY_AUTHORIZATIONS = "authorizations"
 _KEY_VERIFIERS = "verifiers"
 _KEY_AUTHENTICATORS = "authenticators"
+_KEY_ACCOUNTS = "accounts"
 
 _PREFIX_ACCESSCONTROLSERVER = "accesscontrolsrv"
 _PREFIX_AUTHORIZATION = "authorization"
 _PREFIX_VERIFIER = "verifier"
 _PREFIX_AUTHENTICATOR = "authenticator"
+_PREFIX_ACCOUNT = "account"
 
 _POSTFIX_CLIENTUID = "clientuid"
 _POSTFIX_EXPIRATION = "expiration"
@@ -34,6 +36,7 @@ _POSTFIX_STATUS = "status"
 _POSTFIX_MODULE = "module"
 _POSTFIX_VERIFIERS = "verifiers"
 _POSTFIX_AUTHENTICATORS = "authenticators"
+_POSTFIX_ACCOUNTS = "accounts"
 
 _NEW_STATUS = "pending"
 
@@ -51,13 +54,16 @@ class AccessControlServer(datatypes.PersistentObjectServer):
         self._authorizations = datatypes.Index(self, key=_KEY_AUTHORIZATIONS, prefix=prefix,
                                                create=True, overwrite=False)
         self._verifiers = datatypes.Index(self, key=_KEY_VERIFIERS, prefix=prefix,
+                                               create=True, overwrite=False)
         self._authenticators = datatypes.Index(self, key=_KEY_AUTHENTICATORS, prefix=prefix,
                                                create=True, overwrite=False)
+        self._accounts = datatypes.Index(self, key=_KEY_ACCOUNTS, prefix=prefix,
                                          create=True, overwrite=False)
 
     def destroy(self):
 
         # Cleanup Indexes
+        self._accounts.destroy()
         self._authenticators.destroy()
         self._verifiers.destroy()
         self._authorizations.destroy()
@@ -170,17 +176,25 @@ class AccessControlServer(datatypes.PersistentObjectServer):
         # Check membership
         return self._authenticators.is_member(key)
 
+    # Account Methods #
 
+    def accounts_create(self, **kwargs):
 
+        return Account(self, create=True, **kwargs)
 
+    def accounts_get(self, uid=None, key=None):
 
         # Check Args
         if not uid and not key:
             raise TypeError("Requires either uid or key")
 
         # Create
+        return Account(self, create=False, key=key, uid=uid)
 
+    def accounts_list(self):
+        return self._accounts.members
 
+    def accounts_exists(self, uid=None, key=None):
 
         # Check Args
         if not uid and not key:
@@ -195,6 +209,7 @@ class AccessControlServer(datatypes.PersistentObjectServer):
             key = str(uid)
 
         # Check membership
+        return self._accounts.is_member(key)
 
 class Authorization(datatypes.UUIDObject, datatypes.UserMetadataObject):
 
@@ -383,6 +398,49 @@ class Verifier(datatypes.UUIDObject, datatypes.UserMetadataObject):
         actr._verifiers.discard(self.key)
         self._authenticators.discard(actr.key)
 
+    # Account Methods #
+
+    def accounts_by_key(self):
+        """Return Accounts as key strings"""
+        return self._accounts.get_val()
+
+    def accounts_by_uid(self):
+        """Return Accounts as UUID objects"""
+        return set([self._val_to_uid(key) for key in self._accounts])
+
+    def accounts_by_obj(self):
+        """Return Accounts as objects"""
+        return set([self._val_to_obj(key, obj=Account) for key in self._accounts])
+
+    def accounts_is_member(self, val):
+        """Return if Account is member"""
+
+        # Process Val
+        key = self._val_to_key(val, obj=Account)
+
+        # Compute Membership
+        return key in self._accounts
+
+    def accounts_add(self, val):
+        """Add Account"""
+
+        # Process Val
+        acct = self._val_to_obj(val, obj=Account)
+
+        # Add Account
+        self._accounts.add(acct.key)
+        acct._verifiers.add(self.key)
+
+    def accounts_remove(self, val):
+        """Remove Account"""
+
+        # Process Val
+        acct = self._val_to_obj(val, obj=Account)
+
+        # Remove Account
+        acct._verifiers.discard(self.key)
+        self._accounts.discard(acct.key)
+
 class Authenticator(datatypes.UUIDObject, datatypes.UserMetadataObject):
 
     def __init__(self, srv, create=False, overwrite=False,
@@ -449,34 +507,60 @@ class Authenticator(datatypes.UUIDObject, datatypes.UserMetadataObject):
         """Return Verifier Memberships as Objects"""
         return set([self._val_to_obj(key, obj=Verifier) for key in self._verifiers])
 
+class Account(datatypes.UUIDObject, datatypes.UserMetadataObject):
 
     def __init__(self, srv, create=False, overwrite=False,
+                 prefix=_PREFIX_ACCOUNT,
+                 **kwargs):
+        """Initialize Account"""
 
         # Check Input
         datatypes.check_isinstance(srv, AccessControlServer)
         if create:
             pass
         if overwrite:
+            raise TypeError("Account does not support overwrite")
 
         # Call Parent
         super().__init__(srv, create=create, overwrite=overwrite,
                          prefix=prefix, **kwargs)
 
         # Setup Vars
+        self._verifiers = self._build_subobj(dso.MutableSet, _POSTFIX_VERIFIERS,
+                                            create=create, value=set())
 
         # Register with Server
         if create:
+            self.srv._accounts.add(self)
         else:
+            if not self.srv.accounts_exists(key=self.key):
+                msg = "Authorization not associated with srv"
                 raise TypeError(msg)
 
     def destroy(self):
+        """Delete Account"""
 
         # Unregister with Server
+        self.srv._accounts.remove(self)
 
+        # Unregister with Verifiers
+        for verifier in self.verifiers_by_obj():
+            verifier.accounts_remove(self)
 
+        # Cleanup Vars
+        self._verifiers.rem()
 
         # Call Parent
         super().destroy()
 
+    def verifiers_by_key(self):
+        """Return Verifier Memberships as Keys"""
+        return self._verifiers.get_val()
 
+    def verifiers_by_uid(self):
+        """Return Verifier Memberships as UUIDs"""
+        return set([self._val_to_uid(key) for key in self._verifiers])
 
+    def verifiers_by_obj(self):
+        """Return Verifier Memberships as Objects"""
+        return set([self._val_to_obj(key, obj=Verifier) for key in self._verifiers])
