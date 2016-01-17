85a86f5ea8fb382ac7a360bae8e62d7d25901dfe
Andy Sayler <andy.sayler@gmail.com>
2016-01-16 20:11:41 -0700

    Removed unnecessary casts

--- pytutamen_server/utility.py
+++ pytutamen_server/utility.py
@@ -53,8 +53,8 @@ def sign_auth_token(priv_key, clientuid, expiration, objperm, objtype, objuid):
 
     val = { AUTHZ_KEY_CLIENTUID: str(clientuid),
             AUTHZ_KEY_EXPIRATION: int(expiration.timestamp()),
-            AUTHZ_KEY_OBJPERM: str(objperm),
-            AUTHZ_KEY_OBJTYPE: str(objtype),
+            AUTHZ_KEY_OBJPERM: objperm,
+            AUTHZ_KEY_OBJTYPE: objtype,
             AUTHZ_KEY_OBJUID: str(objuid)}
 
     return crypto.sign_jwt(val, priv_key)
@@ -65,9 +65,9 @@ def verify_auth_token(pub_key, token):
     out = crypto.verify_jwt(token, pub_key)
 
     val = { AUTHZ_KEY_CLIENTUID: uuid.UUID(out[AUTHZ_KEY_CLIENTUID]),
-            AUTHZ_KEY_EXPIRATION: datetime.datetime.fromtimestamp(int(out[AUTHZ_KEY_EXPIRATION])),
-            AUTHZ_KEY_OBJPERM: str(out[AUTHZ_KEY_OBJPERM]),
-            AUTHZ_KEY_OBJTYPE: str(out[AUTHZ_KEY_OBJTYPE]),
+            AUTHZ_KEY_EXPIRATION: datetime.datetime.fromtimestamp(out[AUTHZ_KEY_EXPIRATION]),
+            AUTHZ_KEY_OBJPERM: out[AUTHZ_KEY_OBJPERM],
+            AUTHZ_KEY_OBJTYPE: out[AUTHZ_KEY_OBJTYPE],
             AUTHZ_KEY_OBJUID: uuid.UUID(out[AUTHZ_KEY_OBJUID]) }
 
     return val
