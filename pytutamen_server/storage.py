4bf7f312ff8cd903e62cb15b91c8d1a9d9f94c1b
Andy Sayler <andy.sayler@gmail.com>
2016-01-17 22:30:17 -0700

    Added support for ac_servers and ac_required to collections

--- pytutamen_server/storage.py
+++ pytutamen_server/storage.py
@@ -7,6 +7,8 @@
 ### Imports ###
 
 import uuid
+import urllib.parse
+
 
 from . import utility
 from . import datatypes
@@ -23,6 +25,8 @@ _PREFIX_COLLECTION = "collection"
 _PREFIX_SECRET = "secret"
 
 _POSTFIX_DATA = "data"
+_POSTFIX_ACSERVERS = "acservers"
+_POSTFIX_ACREQUIRED = "acrequired"
 
 
 ### Objects ###
@@ -51,14 +55,35 @@ class StorageServer(datatypes.ServerObject):
 
 class Collection(datatypes.UUIDObject, datatypes.UserDataObject, datatypes.ChildObject):
 
-    def __init__(self, pbackend, pindex=None, prefix=_PREFIX_COLLECTION, **kwargs):
+    def __init__(self, pbackend, pindex=None, prefix=_PREFIX_COLLECTION, 
+                 ac_servers=None, ac_required=None, create=False, **kwargs):
         """Initialize Collection"""
 
         # Check Input
         utility.check_isinstance(pindex.parent, StorageServer)
+        if create:
+            utility.check_isinstance(ac_servers, list)
+            utility.check_isinstance(ac_required, int)
+
+            urls = []
+            for url in ac_servers:
+                # Todo: normalize URL
+                p = urllib.parse.urlparse(url)
+                if not p.scheme.lower() == 'https':
+                    raise ValueError("AC URL requires https")
+                if not p.hostname:
+                    raise ValueError("AC URL requires hostname")
+                if p.path:
+                    raise ValueError("AC URL should have no path")
 
         # Call Parent
-        super().__init__(pbackend, pindex=pindex, prefix=prefix, **kwargs)
+        super().__init__(pbackend, pindex=pindex, prefix=prefix, create=create, **kwargs)
+
+        # Setup Objects
+        self._ac_servers = self._build_pobj(self.pcollections.MutableList,
+                                            _POSTFIX_ACSERVERS, create=ac_servers)
+        self._ac_required = self._build_pobj(self.pcollections.MutableString,
+                                             _POSTFIX_ACREQUIRED, create=str(ac_required))
 
         # Setup Secret Index
         self._secrets = datatypes.ChildIndex(self, Secret, _INDEX_KEY_SECRETS)
@@ -69,6 +94,10 @@ class Collection(datatypes.UUIDObject, datatypes.UserDataObject, datatypes.Child
         # Cleanup Indexes
         self._secrets.destroy()
 
+        # Cleanup Objects
+        self._ac_required.rem()
+        self._ac_servers.rem()
+
         # Call Parent
         super().destroy()
 
@@ -76,6 +105,14 @@ class Collection(datatypes.UUIDObject, datatypes.UserDataObject, datatypes.Child
     def server(self):
         """Return Storage Server"""
         return self.parent
+
+    @property
+    def ac_servers(self):
+        return self._ac_servers.get_val()
+
+    @property
+    def ac_required(self):
+        return int(self._ac_required.get_val())
 
     @property
     def secrets(self):
