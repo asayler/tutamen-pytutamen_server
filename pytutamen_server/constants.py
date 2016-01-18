d94353f2bc193ee6585d2c4d023d4502ec062e5c
Andy Sayler <andy.sayler@gmail.com>
2016-01-18 16:02:51 -0700

    Added constants module

---
+++ pytutamen_server/constants.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Andy Sayler
+# Copyright 2015, 2016
+
+
+TYPE_SRV = "server"
+
+PERM_SRV_COL_CREATE = "srv-col-create"
+
+TYPE_COL = "collection"
+
+PERM_COL_CREATE = "col-create"
+PERM_COL_READ = "col-read"
+PERM_COL_MODIFY = "col-modify"
+PERM_COL_DELETE = "col-delete"
+PERM_COL_AC = "col-ac"
