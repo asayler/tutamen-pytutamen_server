45660731b2f045db6f763b8d0a2d13291e9b3bfa
Andy Sayler <andy.sayler@gmail.com>
2016-01-18 21:31:59 -0700

    whitespace

--- pytutamen_server/constants.py
+++ pytutamen_server/constants.py
@@ -5,11 +5,9 @@
 
 
 TYPE_SRV = "server"
-
 PERM_SRV_COL_CREATE = "srv-col-create"
 
 TYPE_COL = "collection"
-
 PERM_COL_CREATE = "col-create"
 PERM_COL_READ = "col-read"
 PERM_COL_MODIFY = "col-modify"
