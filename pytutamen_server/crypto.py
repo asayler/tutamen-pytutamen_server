6ba71fb6cd5c167cfb7674682996064d28c3f766
Andy Sayler <andy.sayler@gmail.com>
2016-01-25 00:06:03 -0700

    Switched to using constant durations

--- pytutamen_server/crypto.py
+++ pytutamen_server/crypto.py
@@ -19,13 +19,10 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 
+from . import constants
+
 
 ### Constants ###
-
-DUR_ONE_DAY = datetime.timedelta(1, 0, 0)
-DUR_ONE_MONTH = datetime.timedelta(28, 0, 0)
-DUR_ONE_YEAR = datetime.timedelta(366, 0, 0)
-DUR_TEN_YEAR = datetime.timedelta(3660, 0, 0)
 
 TYPE_RSA = 'RSA'
 
@@ -117,7 +114,7 @@ def gen_ca_pair(cn, country, state, locality, org, ou, email,
                 length=None, pub_exp=None, typ=None, sig=SIG_SHA256):
 
     if not duration:
-        duration = DUR_TEN_YEAR
+        duration = constants.DUR_TEN_YEAR
     if not serial:
         serial = uuid.uuid4()
     if sig != SIG_SHA256:
@@ -148,7 +145,7 @@ def gen_ca_pair(cn, country, state, locality, org, ou, email,
     builder = x509.CertificateBuilder()
     builder = builder.issuer_name(x509.Name(sub_attr))
     builder = builder.subject_name(x509.Name(sub_attr))
-    builder = builder.not_valid_before(datetime.datetime.today() - DUR_ONE_DAY)
+    builder = builder.not_valid_before(datetime.datetime.today() - constants.DUR_ONE_DAY)
     builder = builder.not_valid_after(datetime.datetime.today() + duration)
     builder = builder.serial_number(int(serial))
     builder = builder.public_key(ca_key.public_key())
@@ -202,7 +199,7 @@ def csr_to_crt(csr_pem, ca_crt_pem, ca_key_pem, password=None,
     if not org:
         org = csr.subject.get_attributes_for_oid(x509.NameOID.ORGANIZATION_NAME)[0].value
     if not duration:
-        duration = DUR_ONE_YEAR
+        duration = constants.DUR_ONE_YEAR
     if not serial:
         serial = uuid.uuid4()
 
@@ -219,7 +216,7 @@ def csr_to_crt(csr_pem, ca_crt_pem, ca_key_pem, password=None,
     builder = x509.CertificateBuilder()
     builder = builder.issuer_name(ca_crt.subject)
     builder = builder.subject_name(x509.Name(sub_attrs))
-    builder = builder.not_valid_before(datetime.datetime.today() - DUR_ONE_DAY)
+    builder = builder.not_valid_before(datetime.datetime.today() - constants.DUR_ONE_DAY)
     builder = builder.not_valid_after(datetime.datetime.today() + duration)
     builder = builder.serial_number(int(serial))
     builder = builder.public_key(csr.public_key())
